071f09f93ec258092b17ebb52874cd40a58d77bf
ErnestoPelayo <ernesto.p@itmexicali.edu.mx>
2024-07-20 16:41:51 -0700

    show url in iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ItemList from "./components/ItemList";
 
 
 function App() {
@@ -17,14 +18,31 @@ function App() {
   }
 
 
-  const getWebsite = async (url:string) => {
+  const getWebsite = (url:string) => {
     setWebsite(url);
   }
   return (
     <>
       <p className=" text-7xl">Uizard Test </p>
-      <div className=" container mx-4 ">
-
+      <div className=" container mx-auto ">
+        <div className="bg-yellow-300 mt-10 rounded-t-md rounded-tr-md">
+          <p className="p-4 text-center font-black ">Uizard Test</p>  
+        </div>
+        <div className=" flex">
+            <div className=" w-1/3 bg-gray-200 p-3" >
+              {
+                data.map((item)=>{
+                  return <ItemList id={item} website={getWebsite}/>
+                })
+              }
+            </div>
+            <div className=" w-2/3 bg-gray-200 p-3">
+              <div className=" w-full flex items-center justify-center h-full">
+                  <iframe src={website} className="w-full h-full p-2"></iframe>
+                </div>
+            </div>
+        
+        </div>
       </div>
     </>
   )
